6c436e4532735e224cd9bc24ebb2d71adfed626e
onlypatric <patric.personal99@gmail.com>
2022-12-13 08:21:45 +0100

    first commit, all stages done

---
+++ README.md
@@ -0,0 +1,34 @@
+## Project details
+
+Author: Pintescul Patric
+Version: 1.0 06/12/2022 (dd/mm/yyyy)
+
+## Project description
+
+1) create a random range between -10 and 9 which expands for 23 hours,
+## Folder Structure
+
+The workspace contains one folder by default, where:
+
+- `src`: the folder to maintain sources
+
+Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+
+```
+Tree directory 
+.
+├── README.md
+└── src
+    ├── MainApp.java
+    └── pkg
+        ├── build
+        └── util
+            ├── RUtil.class
+            └── RUtil.java
+```
+
+> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+
+## Dependency Management
+
+The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
---
+++ src/MainApp.java
@@ -0,0 +1,60 @@
+/**
+ * Progetto Temperature
+ * 
+ * @author Pintescul Patric
+ * @version 1.0
+ */
+import pkg.util.RUtil;
+public class MainApp {
+    public static void main(String[] args) {
+        RUtil r = new RUtil();
+
+        int casuale;
+
+        int subZeroCounter=0;
+        int FivePlusCounter=0;
+
+        int max=-10;
+
+        int limite_giorno=24;
+        
+        //genero 24 (le ore di una giornata) temperature casuali tra -10 e 9 Gradi
+        for ( int i=0; i<limite_giorno; i++){
+            casuale=r.randInt(-10, 9);
+            //System.out.println("Ora: " + i + " Temperatura: " + casuale+"°");
+            // stampa migliorata con l'uso di format
+            if (casuale<0){
+                subZeroCounter++;
+            }
+            else if(casuale>5){
+                FivePlusCounter++;
+            }
+            if(casuale>max){
+                max=casuale;
+            }
+            System.out.format("H: %2d - Deg: %2d°\n",i,casuale);
+        }
+
+        /** 
+         * @todo [difficoltà 1]
+         * dire quante ore della giornata la temperature sono sotto zero (zero gradi esclusi) 
+         * 
+         */
+        System.out.printf("Counted sub-zero temperatures:%5d\n",subZeroCounter);
+    
+        /** 
+         * @todo [difficoltà 2]
+         * dire quante ore della giornata la temperature sono superiori al Gradi 5 ( 5 escluso) 
+         * 
+         */
+        System.out.printf("Counted >5 temperatures:%5d\n",FivePlusCounter);
+        
+        /**
+         * @todo [difficoltà 3]
+         * dire a che ora della giornata c'è stata la temperatura massima
+         * 
+         */
+        System.out.printf("Max temperature registered:%5d\n",max);
+        
+    }
+}
